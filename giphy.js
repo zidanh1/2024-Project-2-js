91ab3130f2ee32e90b3c51e6f989180d62e24bcc
Hashem Zidan <HashemZidan@MacBook-Pro.local>
2024-05-08 16:17:23 -0400

    giphy server data fetch

--- giphy.js
+++ giphy.js
@@ -1,1 +1,21 @@
+const API_KEY = "CoTX4wFXSUZET5AaFKkT0jDNs2pCi8Eq";
+const API_PREFIX = "https://api.giphy.com/v1/gifs/search?api_key=";
+const API_SETTINGS = "&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
+function renderGifs(response) {
+  console.log(response);
+}
+
+function getMemes(searchExpression) {
+  fetch(`${API_PREFIX}${API_KEY}&q=${searchExpression}&limit=25${API_SETTINGS}`)
+    .then((data) => data.json())
+    .then(renderGifs);
+}
+
+function formsubmitted(event) {
+  event.preventDefault();
+  let inputFieldContent = document.querySelector("[name=meme-input]").value;
+  getMemes(inputFieldContent);
+}
+
+document.querySelector("#meme-form").addEventListener("submit", formsubmitted);
