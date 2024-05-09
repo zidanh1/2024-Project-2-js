9fb13af22f51e2877f6d5d022f53d564f03abc9a
Hashem Zidan <HashemZidan@MacBook-Pro.local>
2024-05-09 12:23:22 -0400

    Read me

---
+++ README.md
@@ -0,0 +1,7 @@
+# Giphy API
+
+Hosting link: https://zidanh1.github.io/2024-Project-2-js/?meme-input=+
+
+## User stories
+
+## Wireframe
--- giphy.js
+++ giphy.js
@@ -5,38 +5,33 @@ const API_SETTINGS = "&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 function renderGifs(response) {
   let result = "";
 
-if (response.data.length === 0)
-    renderError ("No results.");
-} else {
-
-
-}
- 
- 
- 
+  if (response.data.length === 0) {
+    renderError("No results.");
+  } else {
     for (let meme of response.data) {
-    result += `
+      result += `
     <img 
     src="${meme.images.original.url}"
      alt="${meme.alt_text}" 
     class="meme-img"  />
     `;
+    }
+
+    document.querySelector(".js-memes-container").innerHTML = result;
   }
+}
 
-  document.querySelector(".js-memes-container").innerHTML = result;
-
-  function renderError(message) {
-    document.querySelector(".js-memes-container").innerHTML = `
+function renderError(message) {
+  document.querySelector(".js-memes-container").innerHTML = `
         <div class="alert alert-danger error-container">${message}</div>
     `;
-  }
-
+}
 
 function getMemes(searchExpression) {
   fetch(`${API_PREFIX}${API_KEY}&q=${searchExpression}&limit=25${API_SETTINGS}`)
     .then((data) => data.json())
     .then(renderGifs)
-    .catch (() => renderError("Error retrieving data.")); 
+    .catch(() => renderError("Error retrieving data."));
 }
 
 function formsubmitted(event) {
